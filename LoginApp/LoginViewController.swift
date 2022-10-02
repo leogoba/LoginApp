52a3e69a1abd3fc439da704fad3abd7870b7d941
leogoba <leogob>
2022-10-02 17:15:12 +0400

    Добавил алерт на неверный логин и пароль

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        if userNameTF.text != "User" || passwordTF.text != "Password" {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please enter correct login or password"
+            )
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 
-
+    
 }
 
